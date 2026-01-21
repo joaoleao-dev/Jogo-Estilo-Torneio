7c1f10dd8a2f984e5b1ee04d61f5cc82d74a5339
João Leão <vtrhavertz@gmail.com>
2026-01-21 10:48:22 -0300

    Update Cacador.java

--- Cacador.java
+++ Cacador.java
@@ -1,9 +1,10 @@
 import java.util.Random;
 
 public class Cacador extends Combatente {
-    private Random random;
+    private final Random random;
+    private int contGolpe = 0;
 
-    public Cacador(String nome, int XP) {
+    public Cacador(String nome, int XP, int PV) {
         super(nome, XP, 50);
         this.random = new Random();
     }
@@ -13,15 +14,16 @@ public class Cacador extends Combatente {
         int danoBase = this.XP * 3; // Exemplo de cálculo de dano
         
         // Verifica se houve acerto crítico (ex: 20% de chance)
-        if (random.nextDouble() < 0.20) {
+        if (random.nextDouble() < 0.20 && this.contGolpe >= 3) {
             int danoCritico = danoBase * 2; // O impacto é dobrado 
             System.out.println(this.nome + " desferiu um ACERTO CRÍTICO em " + alvo.getNome() + "!");
             alvo.receberDano(danoCritico);
+            this.contGolpe = 0;
         } else {
             System.out.println(this.nome + " disparou contra " + alvo.getNome());
             alvo.receberDano(danoBase);
+            this.contGolpe +=1;
         }
     }
 
 }
-
