fd6c798c1abff0edcfb33ce9277a3cb8ce97ede5
João Leão <vtrhavertz@gmail.com>
2026-01-21 10:46:20 -0300

    Update Arcanista.java

--- Arcanista.java
+++ Arcanista.java
@@ -1,15 +1,33 @@
 public class Arcanista extends Combatente {
+    private int magia;
 
     public Arcanista(String nome, int XP, int PV) {
-        super(nome, XP, PV);
-        
+        super(nome, XP, 100);
+        this.magia = 100;
     }
 
     @Override
     public void atacar(Combatente alvo) {
-        int danoMagico = this.XP * 2; // Regra específica do Arcanista
-        System.out.println(this.nome + " lança um feitiço em " + alvo.getNome());
-        alvo.receberDano(danoMagico);
-    }
+        int danoMagico = 100;
+        if(magia >= 20){ 
+            System.out.println(this.nome + " lança um feitiço em " + alvo.getNome());
+            this.magia -= 20;
+            alvo.receberDano(danoMagico);
+        } else {
+            System.out.println(this.nome + " está sem magia! Realizou um ataque fraco e meditou.");
+            alvo.receberDano(20); 
+            this.magia += 20;
+            
+            // GARANTIA: Nunca ultrapassa 100
+            if (this.magia > 100) {
+                this.magia = 100;
+            }
+        }
+        // GARANTIA: Nunca fica menor que 0
+        if (this.magia < 0) {
+            this.magia = 0;
 
+        }
+
+    }
 }
