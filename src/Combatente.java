06de1dfbf7aa10f08a788ed9ff47ee79543f2ee2
João Leão <vtrhavertz@gmail.com>
2026-01-31 04:12:14 -0300

    Update and rename Combatente.java to src/Combatente.java
    refactor: adicionando atributo pvMaximo

---
+++ src/Combatente.java
@@ -0,0 +1,35 @@
+public abstract class Combatente{
+    protected String nome;
+    protected int XP;
+    protected int PV;
+    protected int pvMaximo;
+    
+    public Combatente (String nome, int XP, int pvMaximo) {
+        this.nome = nome;
+        this.XP = XP;
+        this.pvMaximo = pvMaximo
+        this.PV = pvMaximo;
+    }
+    
+    public abstract void atacar(Combatente alvo);
+
+    public boolean estaVivo(){
+        return this.PV > 0;
+    }
+    public String receberDano(int dano) {
+        if (dano < 0) return"";
+        this.PV -= dano;
+        if (this.PV < 0) this.PV = 0;
+            return"";
+    }
+    public String getNome(){
+        return this.nome;
+    }
+    public int PV(){
+        return this.PV;
+    }
+    public int getPvMaximo() {
+        return this.pvMaximo;
+    }
+
+}
