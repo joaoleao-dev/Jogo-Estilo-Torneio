b57a9af70ad6f43dc90dacb1f0acf69c3ed783b1
João Leão <vtrhavertz@gmail.com>
2026-01-31 04:21:12 -0300

    Update and rename Arcanista.java to src/Arcanista.java

--- src/Arcanista.java
+++ src/Arcanista.java
@@ -1,33 +1,30 @@
 public class Arcanista extends Combatente {
     private int magia;
 
-    public Arcanista(String nome, int XP, int PV) {
+    public Arcanista(String nome, int XP) {
         super(nome, XP, 100);
         this.magia = 100;
     }
-
+    public int getMagia(){
+        return this.magia;
+    }
     @Override
-    public void atacar(Combatente alvo) {
+    public String atacar(Combatente alvo) {
         int danoMagico = 100;
         if(magia >= 20){ 
-            System.out.println(this.nome + " lança um feitiço em " + alvo.getNome());
             this.magia -= 20;
             alvo.receberDano(danoMagico);
+            return "✨ " + this.nome + " lança um feitiço em " + alvo.getNome() + "!\n";
         } else {
-            System.out.println(this.nome + " está sem magia! Realizou um ataque fraco e meditou.");
             alvo.receberDano(20); 
             this.magia += 20;
-            
-            // GARANTIA: Nunca ultrapassa 100
             if (this.magia > 100) {
-                this.magia = 100;
+                this.magia = 100; // GARANTIA: Nunca ultrapassa 100
             }
         }
-        // GARANTIA: Nunca fica menor que 0
         if (this.magia < 0) {
-            this.magia = 0;
-
+            this.magia = 0; // GARANTIA: Nunca fica menor que 0
         }
-
+        return "🌀 " + this.nome + " está sem magia! Realizou um ataque fraco e meditou.\n";
     }
 }
